bc2f9ab07203f15934490a50c40defb3e30da3f3
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-07-30 17:58:26 +0900

    #39 自動生成ファイルの対象をanalyzeから除外

--- test/repositories/api_repository_test.dart
+++ test/repositories/api_repository_test.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 
-import 'package:chain/global.dart';
 import 'package:chain/values/token.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 
