9062bace908fb527816751af7dad62844cf6c3fd
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-07-30 22:18:21 +0900

    #42 フォロー・フォロワー数を取得する処理を作成

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,10 +6,13 @@ part 'user.g.dart';
 @freezed
 class User with _$User {
   const factory User({
-    required String? id,
-    required String? name,
-    required String? description,
-    required String? thumbnail,
+    required String id,
+    String? name,
+    String? description,
+    String? thumbnail,
+    @Default(0) int follow,
+    @Default(0) int follower,
+    @Default(0) @JsonKey(name: 'follow_tag') int followTag,
   }) = _User;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
--- test/repositories/user_repository_test.dart
+++ test/repositories/user_repository_test.dart
@@ -26,6 +26,9 @@ void fetchUserTest() {
             "name": "name",
             "description": "description",
             "thumbnail": "thumbnail",
+            "follow": 199,
+            "follower": 200,
+            "follow_tag": 300,
           }));
       final user = await UserRepository(api).fetchSelf();
 
@@ -33,6 +36,9 @@ void fetchUserTest() {
       expect(user.name, "name");
       expect(user.description, "description");
       expect(user.thumbnail, "thumbnail");
+      expect(user.follow, 199);
+      expect(user.follower, 200);
+      expect(user.followTag, 300);
     });
 
     test('should get user when return a user from api 2', () async {
@@ -42,6 +48,9 @@ void fetchUserTest() {
             "name": "name2",
             "description": "description2",
             "thumbnail": "thumbnail2",
+            "follow": 99,
+            "follower": 1000,
+            "follow_tag": 500,
           }));
       final user = await UserRepository(api).fetchSelf();
 
@@ -49,6 +58,24 @@ void fetchUserTest() {
       expect(user.name, "name2");
       expect(user.description, "description2");
       expect(user.thumbnail, "thumbnail2");
+      expect(user.follow, 99);
+      expect(user.follower, 1000);
+      expect(user.followTag, 500);
+    });
+
+    test('should get default follow when return null as follow', () async {
+      final api = MockApiRepository();
+      when(api.get(any)).thenAnswer((_) => Future.value({
+            "id": "id2",
+            "name": "name2",
+            "description": "description2",
+            "thumbnail": "thumbnail2",
+          }));
+      final user = await UserRepository(api).fetchSelf();
+
+      expect(user.follow, 0);
+      expect(user.follower, 0);
+      expect(user.followTag, 0);
     });
 
     test('should give path to api', () async {
@@ -57,17 +84,9 @@ void fetchUserTest() {
         expect(invocation.positionalArguments[0], "users/me");
         return Future.value({
           "id": "id",
-          "name": "name",
-          "description": "description",
-          "thumbnail": "thumbnail",
         });
       });
-      final user = await UserRepository(api).fetchSelf();
-
-      expect(user.id, "id");
-      expect(user.name, "name");
-      expect(user.description, "description");
-      expect(user.thumbnail, "thumbnail");
+      await UserRepository(api).fetchSelf();
     });
   });
 }
