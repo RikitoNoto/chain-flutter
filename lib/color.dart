1fb451d52ed46c7ad392673b30ed91aae32e2d9f
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-08-05 03:36:50 +0900

    #35 色定義ファイルの作成

---
+++ lib/color.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart"
+
+
+const Color kPrimaryColor = Color(0xFFFA8072);
+const Color kSecondaryColor = Colors.red;
+const Color kBackgroundColor = Color(0xFF909090);
+const Color kHeaderColor = Color(0xFFE6E6E6);
+
