36e881c86f351f82bb262ee7bbaecba98c9cde79
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-08-05 13:44:21 +0900

    #35 color schemesファイルの導入

--- lib/color.dart
+++
@@ -1,8 +0,0 @@
-import "package:flutter/material.dart"
-
-
-const Color kPrimaryColor = Color(0xFFFA8072);
-const Color kSecondaryColor = Colors.red;
-const Color kBackgroundColor = Color(0xFF909090);
-const Color kHeaderColor = Color(0xFFE6E6E6);
-
